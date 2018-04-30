ad273e092421d3b1ef484c9295f9c1221aaeff3e
Astrian <byron@byrondavis.net>
2018-04-30 09:08:57 -0700

    Pause Menu
    I'm still working on it, but I did a lot.

---
+++ Assets/Scripts/On Screen/Pause Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resume : MonoBehaviour {
+
+
+	public void DisableBoolAnimator (Animator Resume) {
+
+		Resume.SetBool ("Is Disabled", true);
+
+	}
+
+	public void EnableBoolAnimator (Animator Resume) {
+
+		Resume.SetBool ("Is Disabled", false);
+
+	}
+
+	public void NavigateTo (int scene){
+
+		Application.LoadLevel (scene);
+
+	}
+
+	public void ExitGame (){
+
+		Application.Quit ();
+
+	}
+
+
+}
