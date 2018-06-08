d4b419bb16d64f81c34fb7c43b2ace60954bf904
Astrian <byron@byrondavis.net>
2018-06-08 09:11:10 -0700

    Fight

--- Assets/Scripts/Character Movement/PlayerMovement.cs
+++ Assets/Scripts/Character Movement/PlayerMovement.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float moveSpeed;
+	public float attackTime;
 
 	private Animator anim;
+	private Rigidbody2D myRigidbody;
 	private bool playerMoving;
 	private Vector2 lastMove;
+	private bool attack;
+	private float attackTimeCounter;
 
 	void Start () {
 		anim = GetComponent<Animator> ();
 	}
-		
+
 	void Update () {
 
 		playerMoving = false;
 
-		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f ) {
+		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) {
 
 			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
 			playerMoving = true;
@@ -32,10 +36,34 @@ public class PlayerMovement : MonoBehaviour {
 			lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
 		}
 
-		anim.SetFloat ("MoveX", Input.GetAxisRaw("Horizontal"));
+		if (Input.GetAxisRaw ("Horizontal") < 0.5f && Input.GetAxisRaw ("Horizontal") > -0.5f) {
+
+			myRigidbody.velocity = new Vector2 (Of, myRigidbody.velocity.y);
+
+		}
+
+		if (Input.GetAxisRaw ("Vertical") < 0.5f && Input.GetAxisRaw ("Vertical") > -0.5f) {
+
+			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, Of);
+
+		}
+			
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			attackTimeCounter = attackTime;
+			attack = true;
+			myRigidbody.velocity = Vector2.zero;
+			anim.SetBool ("Attack", true);
+
+		}
+
+
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
 		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
 		anim.SetBool ("PlayerMoving", playerMoving);
 		anim.SetFloat ("LastMoveX", lastMove.x);
 		anim.SetFloat ("LastMoveY", lastMove.y);
+		anim.SetBool ("Attack", attack);
 	}
 }
