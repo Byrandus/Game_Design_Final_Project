7ede02962c7cb1e44bd42906f8ce654873f5b15f
Astrian <byron@byrondavis.net>
2018-06-11 21:22:15 -0700

    Lots of small changes

--- Assets/Scripts/Character Movement/PlayerMovement.cs
+++ Assets/Scripts/Character Movement/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	void Start () {
 		anim = GetComponent<Animator> ();
+        myRigidbody = GetComponent<Rigidbody2D>();
 	}
 
 	void Update () {
@@ -24,27 +25,29 @@ public class PlayerMovement : MonoBehaviour {
 
 		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) {
 
-			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-			playerMoving = true;
+            //transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+            playerMoving = true;
 			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
 		}
 
 		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f ) {
 
-			transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
-			playerMoving = true;
+            //transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            playerMoving = true;
 			lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
 		}
 
 		if (Input.GetAxisRaw ("Horizontal") < 0.5f && Input.GetAxisRaw ("Horizontal") > -0.5f) {
 
-			myRigidbody.velocity = new Vector2 (Of, myRigidbody.velocity.y);
+			myRigidbody.velocity = new Vector2 (0f, myRigidbody.velocity.y);
 
 		}
 
 		if (Input.GetAxisRaw ("Vertical") < 0.5f && Input.GetAxisRaw ("Vertical") > -0.5f) {
 
-			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, Of);
+			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, 0f);
 
 		}
 			
